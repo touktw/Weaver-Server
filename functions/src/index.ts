c7d1bff6eb34c76c9bb8fff8787960322d8a609e
touktw <touktw@gmail.com>
2020-03-25 02:06:43 +0900

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,7 +20,10 @@ const app = express()
 app.use(cors({ origin: true }))
 
 export const v1 = functions.https.onRequest(app)
-const dummyArtists = [new Artist("artist1", 1), new Artist("artist2", 2), new Artist("artist3", 3)]
+const dummyArtists = [
+  new Artist("BTS", "Big Hit Entertainment", 1, 1, "https://firebasestorage.googleapis.com/v0/b/weverse-shop.appspot.com/o/logo_bts.png?alt=media&token=c8eda21b-7a05-49d4-839b-aa6857a2efeb"),
+  new Artist("TOMORROW X TOGETHER", "Big Hit Entertainment", 2, 1, "https://firebasestorage.googleapis.com/v0/b/weverse-shop.appspot.com/o/logo_t_by_t.png?alt=media&token=6b240b8a-0ee1-451c-b8b5-508a9faf4c46"),
+  new Artist("GFRIEND", "Source Music", 3, 2, "https://firebasestorage.googleapis.com/v0/b/weverse-shop.appspot.com/o/logo_gfriend.png?alt=media&token=ad01e2f8-3575-4064-af2b-8b571d42f02e")]
 
 let dummyProducts = new Map<string, Product[]>()
 
