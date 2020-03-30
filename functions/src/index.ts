489a955b3044dade16d0b13fad4753c08050e2d9
tae.kim <tae.kim@neofect.com>
2020-03-30 18:18:42 +0900

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,7 @@ import * as admin from 'firebase-admin';
 import { Artist } from './Artist'
 import { Product } from './Product'
 import { ResultDto } from './ResultDto'
-import { supportedLocales } from './Locales'
-import { DummyProducts, DummyPromotions, DummyNotice } from './Dummy'
+import { DummyProducts, DummyPromotions, DummyNotice, DummyCurrencies, DummyLocales, DummyArtists } from './Dummy'
 import { Promotion } from './Promotion';
 
 
@@ -26,28 +25,28 @@ const bucket = gcs.bucket
 app.use(cors({ origin: true }))
 
 export const v1 = functions.https.onRequest(app)
-const dummyArtists = [
-  new Artist("BTS", "Big Hit Entertainment", 1, 1, "https://firebasestorage.googleapis.com/v0/b/weverse-shop.appspot.com/o/logo_bts.png?alt=media&token=c8eda21b-7a05-49d4-839b-aa6857a2efeb"),
-  new Artist("TOMORROW X TOGETHER", "Big Hit Entertainment", 2, 1, "https://firebasestorage.googleapis.com/v0/b/weverse-shop.appspot.com/o/logo_t_by_t.png?alt=media&token=6b240b8a-0ee1-451c-b8b5-508a9faf4c46"),
-  new Artist("GFRIEND", "Source Music", 3, 2, "https://firebasestorage.googleapis.com/v0/b/weverse-shop.appspot.com/o/logo_gfriend.png?alt=media&token=ad01e2f8-3575-4064-af2b-8b571d42f02e")
-]
+
 
 
 
 app.get('/artists', (req, res) => {
-  const data = JSON.stringify(dummyArtists)
-
   res.send(JSON.stringify({
     "description": "success",
-    "data": data
+    "data": DummyArtists
   }))
 })
 
 app.get('/locales', (req, res) => {
-
   res.send(JSON.stringify({
     "description": "success",
-    "data": supportedLocales
+    "data": DummyLocales
+  }))
+})
+
+app.get('/currencies', (req, res) => {
+  res.send(JSON.stringify({
+    "description": "success",
+    "data": DummyCurrencies
   }))
 })
 
